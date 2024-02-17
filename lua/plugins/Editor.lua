2ab2bfde6daf046134f160f574ee3ba04378c28c
Tetrapak0 <112382253+Tetrapak0@users.noreply.github.com>
2024-02-17 14:23:39 +0100

    remove '<>' autopair

--- lua/plugins/Editor.lua
+++ lua/plugins/Editor.lua
@@ -57,12 +57,10 @@ return {
                 ['('] = {action = "open", pair = "()", neigh_pattern = "[^\\]."},
                 ['['] = {action = "open", pair = "[]", neigh_pattern = "[^\\]."},
                 ['{'] = {action = "open", pair = "{}", neigh_pattern = "[^\\]."},
-                ['<'] = {action = "open", pair = "<>", neigh_pattern = "[^\\]."},
 
                 [')'] = {action = "close", pair = "()", neigh_pattern = "[^\\]."},
                 [']'] = {action = "close", pair = "[]", neigh_pattern = "[^\\]."},
                 ['}'] = {action = "close", pair = "{}", neigh_pattern = "[^\\]."},
-                ['>'] = {action = "close", pair = "<>", neigh_pattern = "[^\\]."},
 
                 ['"'] = {action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = {cr = false}},
                 ["'"] = {action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = {cr = false}},
